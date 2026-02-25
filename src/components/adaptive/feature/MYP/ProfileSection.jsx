25095e460e3fa9e6d36c0cec997acb3752da9d51
wonyeoe <yeowonlee04@gmail.com>
2026-02-25 21:15:02 +0900

    style: 아바타 정가운데에 위치하게

--- src/components/adaptive/feature/MYP/ProfileSection.jsx
+++ src/components/adaptive/feature/MYP/ProfileSection.jsx
@@ -84,7 +84,7 @@ const ProfileSection = ({ onEditMajor }) => {
                     <svg
                         className="w-10 h-10 md:w-12 md:h-12 mt-2"
                         fill="currentColor"
-                        viewBox="0 0 24 24"
+                        viewBox="0 2.5 24 24"
                     >
                         <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
                     </svg>
